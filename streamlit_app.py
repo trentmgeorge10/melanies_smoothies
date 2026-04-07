d4bce0652ad303b9e24fcfe290d588d917b3f8b1
trentmgeorge10 <george.trenton10@gmail.com>
2026-04-07 12:46:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -45,9 +45,9 @@ if ingredients_list:
 
         st.success(""" Your Smoothie is ordered, """ + name_on_order + """ """, icon="✅")
 
-  
+import requests
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
