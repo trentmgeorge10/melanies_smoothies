8f6dccfce02965bcddc2f077d742009900054ed6
trentmgeorge10 <george.trenton10@gmail.com>
2026-04-07 12:49:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         st.success(""" Your Smoothie is ordered, """ + name_on_order + """ """, icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("[http://my.smoothiefroot.com/api/fruit/watermelon](http://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
 
 
